e748b630b0d26c3d9a1fcccbf25c784abfd95dc2
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-11 16:52:14 +0300

    Initial Commit.

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- Image and text -->
+    <nav class="navbar navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">
+        <img src="./assets/brand.png" width="30" height="30" class="d-inline-block align-top" alt="Logo">
+        TADB - The Anime Database
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
