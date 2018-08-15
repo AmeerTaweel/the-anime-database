ff591cf716586e2670696bd076d3bd1f9240940d
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-15 17:18:17 +0300

    Add horizontal rule.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" class="container">
     <h1 class="text-center m-20"><span class="text-primary">T</span>he <span class="text-primary">A</span>nime <span class="text-primary">D</span>atabase</h1>
     <h1 class="text-center"><small class="text-muted">All Your Anime Need At One Place!</small></h1>
+    <hr/>
     <router-view></router-view>
   </div>
 </template>
@@ -18,5 +19,8 @@ export default {
 <style>
 .m-20 {
   margin: 20px;
+}
+hr {
+  border-top: 1px solid #8c8b8b;
 }
 </style>
