e95b96a4d765d4d99fb273237a7308bf0be3a594
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-16 20:01:40 +0300

    Add return to home trigger on the main title.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <h1 class="text-center m-4"><span class="text-primary">T</span>he <span class="text-primary">A</span>nime <span class="text-primary">D</span>atabase</h1>
+    <h1 class="text-center m-4 home" @click="goHome" title="Return To Homepage"><span class="text-primary">T</span>he <span class="text-primary">A</span>nime <span class="text-primary">D</span>atabase</h1>
     <h1 class="text-center"><small class="text-muted">All Your Anime Need At One Place!</small></h1>
     <hr/>
     <router-view></router-view>
@@ -10,8 +10,13 @@
 <script>
 export default {
   name: 'app',
+  methods: {
+    goHome(){
+      this.$router.push('/home')
+    }
+  },
   created(){
-    this.$router.push('/home')
+    this.goHome()
   }
 }
 </script>
@@ -19,5 +24,8 @@ export default {
 <style>
 hr {
   border-top: 1px solid #8c8b8b;
+}
+.home {
+  cursor: pointer;
 }
 </style>
