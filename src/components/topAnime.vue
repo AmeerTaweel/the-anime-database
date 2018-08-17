6d9e2d386bc276980b709b753c4392a467c5170e
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-17 19:11:05 +0300

    Check for unvalid types and redirect them to 404 top anime component.

--- src/components/topAnime.vue
+++ src/components/topAnime.vue
@@ -1,3 +1,37 @@
 <template>
-    <h1>Top Anime</h1>
+    <div id="top-anime">
+        <h1>Top Anime</h1>
+        {{type}}
+    </div>
 </template>
+
+<script>
+export default {
+    name: `top-anime`,
+    data(){
+        return {
+            type: this.$route.params.type
+        }
+    },
+    watch: {
+        '$route': 'updateType'
+    },methods: {
+        updateType(){
+            this.type = this.$route.params.type
+            this.checkType()
+        },
+        goTo404(){
+            this.$router.push('/404/page-not-found')
+        },
+        checkType(){
+            const validTypes = [`airing`, `upcoming`, `movie`, `tv`]
+            if(!validTypes.includes(this.type)){
+                this.goTo404()
+            }
+        }
+    },
+    created(){
+        this.checkType()
+    }
+}
+</script>
