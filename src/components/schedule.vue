dd940dbf3d9cd4715b02531c224ce9f64c8af9fb
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-19 17:38:33 +0300

    Check day vlidity in schedule component.

--- src/components/schedule.vue
+++ src/components/schedule.vue
@@ -3,3 +3,52 @@
         YattaZo
     </div>
 </template>
+
+<script>
+export default {
+    name: `schedule`,
+    data(){
+        return {
+            day: this.$route.params.day,
+            days: [`all`, `monday`, `tuesday`, `wednesday`, `thursday`, `friday`, `saturday`, `sunday`],
+            schedule: []
+        }
+    },
+    watch: {
+        '$route' : 'update'
+    },
+    methods: {
+        checkDay(){
+            if(!this.days.includes(this.day)){
+                this.goTo404()
+                return false
+            }
+            return true
+        },
+        goTo404(){
+            this.$router.push('/404/page-not-found')
+        },
+        update(){
+            this.day = this.$route.params.day
+            if(this.checkDay()){
+                this.getSchedule()
+            }
+        },
+        getSchedule(){
+            let fetchDay = this.day
+            fetch(`https://api.jikan.moe/schedule/${fetchDay}`, {
+                method: `GET`
+            }).then((response) => {
+                return response.json()
+            }).then((jsonResponse) => {
+
+            })
+        }
+    },
+    created(){
+        if(this.checkDay()){
+            this.getSchedule()
+        }
+    }
+}
+</script>
