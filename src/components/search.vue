0c0be17a186e91cc8b294e2edddb5a5639961f51
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-22 14:49:08 +0300

    Create the search component.

---
+++ src/components/search.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="search">
+        {{query}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: `search`,
+    data(){
+        return {
+            query: this.$route.params.search_query
+        }
+    }
+}
+</script>
