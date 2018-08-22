f02d8a1be4b2d22222af71e1ee4bb7703fde108b
Ameer Taweel <ameertaweel2002@gmail.com>
2018-08-22 14:33:55 +0300

    Clicking on the website title won't redirect the user to homepage.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <h1 class="text-center m-4 pointer" @click="goHome" title="Return To Homepage"><span class="text-primary">T</span>he <span class="text-primary">A</span>nime <span class="text-primary">D</span>atabase</h1>
+    <h1 class="text-center m-4"><span class="text-primary">T</span>he <span class="text-primary">A</span>nime <span class="text-primary">D</span>atabase</h1>
     <h1 class="text-center"><small class="text-muted">All Your Anime Need At One Place!</small></h1>
     <nav class="navbar navbar-expand-md navbar-dark bg-primary">
       <router-link class="navbar-brand" to="/home">T.A.D.B.</router-link>
