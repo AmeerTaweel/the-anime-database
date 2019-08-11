c05a0c7030a2de9db779b8d85f3723390fcd95f6
Ameer Taweel <ameertaweel2002@gmail.com>
2019-08-11 22:10:16 +0300

    Create anime view

--- src/router.js
+++ src/router.js
@@ -13,12 +13,12 @@ export default new Router({
 			component: Home
 		},
 		{
-			path: `/about`,
-			name: `about`,
+			path: `/anime/:id`,
+			name: `anime`,
 			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
+			// this generates a separate chunk (anime.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
-			component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+			component: () => import(/* webpackChunkName: "anime" */ './views/Anime.vue')
 		},
 		{
 			path: `*`,
