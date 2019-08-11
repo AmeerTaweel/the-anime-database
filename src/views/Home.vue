810937fee0cce195f4895736d16b7ef947ded469
Ameer Taweel <ameertaweel2002@gmail.com>
2019-08-11 10:23:56 +0300

    Fetch top animes list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,42 @@
 <template>
-	<HelloWorld />
+	<div>
+		
+	</div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
 export default {
-	components: {
-		HelloWorld,
+	name: `App`,
+	data: () => ({
+		animeTypes: {
+			airing: {
+				anime: [],
+				display: `Airing`,
+				type: `airing`
+			}, 
+			upcoming: {
+				anime: [],
+				display: `Upcoming`,
+				type: `upcoming`
+			}
+		}
+	}),
+	methods: {
+		fetchTopAnimesByType(animeType) {
+			// Just get the first page
+			fetch(`https://api.jikan.moe/top/anime/1/${animeType}`, {
+                method: `GET`
+            }).then(response => {
+                return response.json()
+            }).then(jsonResponse => {
+				this.animeTypes[animeType] = jsonResponse.top.slice(0, 24)
+			})
+		}
 	},
+	created() {
+		Object.keys(this.animeTypes).forEach(animeType => {
+			this.fetchTopAnimesByType(animeType)
+		})
+	}
 };
 </script>
