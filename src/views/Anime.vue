0fcafa198fc341d7706241a42b0dfe413ffaec5c
Ameer Taweel <ameertaweel2002@gmail.com>
2019-08-12 08:46:17 +0300

    Finish anime info view

--- src/views/Anime.vue
+++ src/views/Anime.vue
@@ -1,8 +1,8 @@
 <template>
-	<v-container fluid>	
+	<v-container v-bind="{ [`grid-list-md`]: true }" fluid>	
 		<h1 class="display-3 ma-4">{{anime.title}}</h1>
-		<v-layout class="ma-4">
-			<v-flex xs4>
+		<v-layout class="ma-4 text-justify" wrap>
+			<v-flex xs12 md4>
 				<v-layout column>
 					<v-flex xs12>
 						<lazy-component class="fill">
@@ -38,19 +38,35 @@
 							<strong>Broadcast</strong>: {{anime.broadcast || na}}
 						</h5>
 						<h5 class="headline my-2">
-							<strong>Producers</strong>: <span v-for="(producer, index) in anime.producer" :key="index">{{producer.name}}<span v-if="anime.producer.length > index + 1">, </span></span><span v-if="anime.producer.length < 1">{{na}}</span>
+							<strong>Producers</strong>:
+							<span v-if="!anime.producer || !anime.producer.length">{{na}}</span>
+							<div v-else>
+								<span v-for="(producer, index) in anime.producer" :key="index">{{producer.name}}<span v-if="anime.producer.length > index + 1">, </span></span>
+							</div>
 						</h5>
 						<h5 class="headline my-2">
-							<strong>Licensors</strong>: <span v-for="(licensor, index) in anime.licensor" :key="index">{{licensor.name}}<span v-if="anime.licensor.length > index + 1">, </span></span><span v-if="anime.licensor.length < 1">{{na}}</span>
+							<strong>Licensors</strong>:
+							<span v-if="!anime.licensor || !anime.licensor.length">{{na}}</span>
+							<div v-else>
+								<span v-for="(licensor, index) in anime.licensor" :key="index">{{licensor.name}}<span v-if="anime.licensor.length > index + 1">, </span></span>
+							</div>
 						</h5>
 						<h5 class="headline my-2">
-							<strong>Studios</strong>: <span v-for="(studio, index) in anime.studio" :key="index">{{studio.name}}<span v-if="anime.studio.length > index + 1">, </span></span><span v-if="anime.studio.length < 1">{{na}}</span>
+							<strong>Studios</strong>:
+							<span v-if="!anime.studio || !anime.studio.length">{{na}}</span>
+							<div v-else>
+								<span v-for="(studio, index) in anime.studio" :key="index">{{studio.name}}<span v-if="anime.studio.length > index + 1">, </span></span>
+							</div>
 						</h5>
 						<h5 class="headline my-2">
 							<strong>Source</strong>: {{anime.source || na}}
 						</h5>
 						<h5 class="headline my-2">
-							<strong>Genres</strong>: <span v-for="(genre, index) in anime.genre" :key="index">{{genre.name}}<span v-if="anime.genre.length > index + 1">, </span></span><span v-if="anime.genre.length < 1">{{na}}</span>
+							<strong>Genres</strong>:
+							<span v-if="!anime.genre || !anime.genre.length">{{na}}</span>
+							<div v-else>
+								<span v-for="(genre, index) in anime.genre" :key="index">{{genre.name}}<span v-if="anime.genre.length > index + 1">, </span></span>
+							</div>
 						</h5>
 						<h5 class="headline my-2">
 							<strong>Duration</strong>: {{anime.duration || na}}
@@ -58,7 +74,7 @@
 					</v-flex>
 				</v-layout>
 			</v-flex>
-			<v-flex xs8>
+			<v-flex xs12 md8>
 				<v-layout column>
 					<v-flex xs12>
 						<h4 class="display-1 my-5">Statistics:</h4>
@@ -77,32 +93,32 @@
 							</v-flex>
 						</v-layout>
 						<h4 class="display-1 my-5">Synopsis:</h4>
-						<p class="headline my-2 text-justify">{{anime.synopsis}}</p>
+						<p class="headline my-2">{{anime.synopsis}}</p>
 						<div v-if="anime.background">
 							<h4 class="display-1 my-5">Background:</h4>
-							<p class="headline my-2 text-justify">{{anime.background}}</p>
+							<p class="headline my-2">{{anime.background}}</p>
 						</div>
-						<div v-if="(anime.related.Adaptation && anime.related.Adaptation.length > 0) || (anime.related[`Side story`] && anime.related[`Side story`].length > 0) || (anime.related.Summary && anime.related.Summary.length > 0) || (anime.related[`Parent story`] && anime.related[`Parent story`].length > 0)">
-							<h4 class="display-1 my-5">Related Anime:</h4>
-							<h5 class="headline my-2" v-if="anime.related.Adaptation">
+						<div v-if="anime.related">
+							<h4 class="display-1 my-5">Related:</h4>
+							<h5 class="headline my-2" v-if="anime.related.Adaptation && anime.related.Adaptation.length">
 								<strong>Adaptation</strong>: <span v-for="(adaptation, index) in anime.related.Adaptation" :key="index">{{adaptation.title}}<span v-if="anime.related.Adaptation.length > index + 1">, </span></span>
 							</h5>
-							<h5 class="headline my-2" v-if="anime.related[`Side story`]">
+							<h5 class="headline my-2" v-if="anime.related[`Side story`] && anime.related[`Side story`].length">
 								<strong>Side Story</strong>: <span v-for="(sideStory, index) in anime.related[`Side story`]" :key="index">{{sideStory.title}}<span v-if="anime.related[`Side story`].length > index + 1">, </span></span>
 							</h5>
-							<h5 class="headline my-2" v-if="anime.related.Summary">
+							<h5 class="headline my-2" v-if="anime.related.Summary && anime.related.Summary.length">
 								<strong>Summary</strong>: <span v-for="(summary, index) in anime.related.Summary" :key="index">{{summary.title}}<span v-if="anime.related.Summary.length > index + 1">, </span></span>
 							</h5>
-							<h5 class="headline my-2" v-if="anime.related[`Parent story`]">
+							<h5 class="headline my-2" v-if="anime.related[`Parent story`] && anime.related[`Parent story`].length">
 								<strong>Parent Story</strong>: <span v-for="(parentStory, index) in anime.related[`Parent story`]" :key="index">{{parentStory.title}}<span v-if="anime.related[`Parent story`].length > index + 1">, </span></span>
 							</h5>
 						</div>
-						<div v-if="anime.opening_theme && anime.opening_theme.length > 0">
+						<div v-if="anime.opening_theme && anime.opening_theme.length">
 							<h4 class="display-1 my-5">Opening Themes:</h4>
 							<h5 class="headline my-2" v-for="(theme, index) in anime.opening_theme" :key="index">{{theme}}</h5>
 						</div>
-						<div v-if="anime.ending_theme && anime.ending_theme.length > 0">
-							<h4 class="display-1 my-5">Ending Theme:</h4>
+						<div v-if="anime.ending_theme && anime.ending_theme.length">
+							<h4 class="display-1 my-5">Ending Themes:</h4>
 							<h5 class="headline my-2" v-for="(theme, index) in anime.ending_theme" :key="index">{{theme}}</h5>
 						</div>
 					</v-flex>
